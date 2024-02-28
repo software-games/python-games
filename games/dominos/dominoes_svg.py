01d48abf074f1a4b048da409b2ea8536962bd64d
Christian Clauss <cclauss@me.com>
2024-02-28 19:25:11 +0100

    Update dominoes_svg.py

--- games/dominos/dominoes_svg.py
+++ games/dominos/dominoes_svg.py
@@ -60,8 +60,8 @@ def svg_domino(svg: Drawing, domino: tuple[int, int], width_in_px: int = 200) ->
     """
     size = (width_in_px, width_in_px * 2)
     svg.add(svg.rect(insert=(2, 2), size=size, fill="white", stroke="blue"))
-    x1 = 2 + pip_offsets[0] * w - 5  # Left of the left-most pip
-    x2 = 2 + pip_offsets[2] * w + 5  # Right of the right-most pip
+    x1 = 2 + pip_offsets[0] * width_in_px - 5  # Left of the left-most pip
+    x2 = 2 + pip_offsets[2] * width_in_px + 5  # Right of the right-most pip
     svg.add(svg.line(x1=x1, y1=width_in_px, x2=x2, y2=width_in_px, stroke="blue"))
     for die, pip_count in enumerate(domino):
         svg_die(svg, die, pip_count, width_in_px)
