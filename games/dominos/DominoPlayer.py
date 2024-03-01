d1f0247c343884870185fb2cd713c52cfea43fd7
Christian Clauss <cclauss@me.com>
2024-03-01 12:44:40 +0100

    Add pytests (#28)
    * Add pytests
    
    * Add pytests
    
    * Add pytests
    
    * Update ci.yml
    
    * Update ci.yml

--- games/dominos/DominoPlayer.py
+++ games/dominos/DominoPlayer.py
@@ -25,6 +25,29 @@ class DominoRunMove:
         return f"{s} on {self.mAlreadyPlayedDomino.mDomino} {theMessage}"
 
 
+class TestDominoRunMove:
+    """
+    Tests for the DominoRunMove class
+    """
+
+    def test_init(self):
+        """
+        Test the __init__ method
+        """
+        played_domino = PlayedDomino(1, [0, 0])
+        run_move = DominoRunMove(played_domino, [0, 1])
+        assert run_move.mAlreadyPlayedDomino == played_domino
+        assert run_move.mToBePlayedSimpleDomino == [0, 1]
+
+    def test_str(self):
+        """
+        Test the __str__ method
+        """
+        played_domino = PlayedDomino(1, [0, 0])
+        run_move = DominoRunMove(played_domino, [0, 1])
+        assert str(run_move) == "Playing [0, 1] on [0, 0] "
+
+
 class DominoPlayer:
     def __init__(self, name: str, board: DominoBoard) -> None:
         self.name = name
@@ -282,6 +305,156 @@ class DominoPlayer:
                 bestScoreAndRun[1] = theRun + theScoreAndRun[1]
         self.board.mPlayedDominos.pop().notifyNeighborsOfUndo()
         return bestScoreAndRun
+
+
+class TestDominoPlayer:
+    """
+    Tests for the DominoPlayer class
+    """
+
+    def test_init(self):
+        """
+        Test the __init__ method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        assert player.name == "Player 1"
+        assert player.board == board
+        assert player.points == 0
+        assert player.dominos == []
+        assert player.go_agains == 0
+        assert player.hands_won == 0
+        assert player.games_won == 0
+        assert player.is_player_human is False
+
+    def test_str(self):
+        """
+        Test the __str__ method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        assert (
+            str(player)
+            == "Player 1 has 0 dominos, 0 points, 0 go agains, 0 hands and 0 games won."
+        )
+
+    def test_hand_as_str(self):
+        """
+        Test the hand_as_str method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        assert player.hand_as_str() == "Player 1's hand: 0 []"
+
+    def test_award_points(self):
+        """
+        Test the award_points method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.award_points(10)
+        assert player.points == 10
+
+    def test_points_still_holding(self):
+        """
+        Test the points_still_holding method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.dominos = [[0, 0], [0, 1], [1, 1]]
+        assert player.points_still_holding() == 3
+
+    def test_isDominoPlayable(self):
+        """
+        Test the isDominoPlayable method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.dominos = [[0, 0], [0, 1], [1, 1]]
+        assert player.isDominoPlayable(0) is True
+        assert player.isDominoPlayable(1) is True
+        assert player.isDominoPlayable(2) is True
+        # TODO: Add a played domino
+
+    def test_starIfPlayable(self):
+        """
+        Test the starIf
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.dominos = [[0, 0], [0, 1], [1, 1]]
+        assert player.starIfPlayable(0) == "*"
+        assert player.starIfPlayable(1) == "*"
+        assert player.starIfPlayable(2) == "*"
+        # TODO: Add a played domino
+
+    def test_canPlay(self):
+        """
+        Test the canPlay method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.dominos = [[0, 0], [0, 1], [1, 1]]
+        assert player.canPlay() is True  # TODO: Add a False case
+
+    def test_pickFromBoneyard(self):
+        """
+        Test the pickFromBoneyard method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        assert player.dominos == []
+        player.pickFromBoneyard()
+        assert len(player.dominos) == 0  # TODO: Fix me
+
+    def test_pickFromBoneyardUntilCanPlay(self):
+        """
+        Test the pickFromBoneyardUntilCanPlay method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.dominos = [[0, 0], [0, 1], [1, 1]]
+        assert player.pickFromBoneyardUntilCanPlay() is True
+
+    def test_undo(self):
+        """
+        Test the undo method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.board.mPlayedDominos = [PlayedDomino(1, [0, 0])]
+        # player.board.points = 10  # TODO: Fix me
+        # player.undo()
+        # assert player.points == 0
+        # assert player.board.mPlayedDominos == []
+
+    def test_getFreshCopy(self):
+        """
+        Test the getFreshCopy method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        played_domino = PlayedDomino(1, [0, 0])
+        player.getFreshCopy(played_domino)
+        # assert player.dominos == []  # TODO: Fix me
+
+    def test_playARun(self):
+        """
+        Test the playARun method
+        """
+        board = DominoBoard()
+        _player = DominoPlayer("Player 1", board)
+        _run = [DominoRunMove(PlayedDomino(1, [0, 0]), [0, 1])]
+        # assert player.playARun(run) == False  # TODO: Fix me
+
+    def test_playAMove(self):
+        """
+        Test the playAMove method
+        """
+        board = DominoBoard()
+        player = DominoPlayer("Player 1", board)
+        player.dominos = [[0, 0], [0, 1], [1, 1]]
+        # assert player.playAMove(DominoRunMove(PlayedDomino(1, [0, 0]), [0, 1])) == False  # TODO: Fix me
 
 
 if __name__ == "__main__":
--- games/dominos/DominoWorld.py
+++ games/dominos/DominoWorld.py
@@ -2,6 +2,8 @@
 
 from random import shuffle
 
+import pytest
+
 from DominoBoard import DominoBoard
 
 # from sys import argv
@@ -16,8 +18,62 @@ def initDominos(inMaxDie=6) -> list[list[int]]:
     return [[x, y] for x in range(inMaxDie + 1) for y in range(x, inMaxDie + 1)]
 
 
+def test_initDominos():
+    assert initDominos() == [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 4],
+        [0, 5],
+        [0, 6],
+        [1, 1],
+        [1, 2],
+        [1, 3],
+        [1, 4],
+        [1, 5],
+        [1, 6],
+        [2, 2],
+        [2, 3],
+        [2, 4],
+        [2, 5],
+        [2, 6],
+        [3, 3],
+        [3, 4],
+        [3, 5],
+        [3, 6],
+        [4, 4],
+        [4, 5],
+        [4, 6],
+        [5, 5],
+        [5, 6],
+        [6, 6],
+    ]
+
+
 def pointsRounded(inValue, n=5) -> int:
     return int(round((inValue + n // 2) // n))
+
+
+@pytest.mark.parametrize(
+    "inValue, expected",
+    [
+        (0, 0),
+        (1, 0),
+        (2, 0),
+        (3, 1),
+        (7, 1),
+        (8, 2),
+        (12, 2),
+        (13, 3),
+        (17, 3),
+        (18, 4),
+        (22, 4),
+        (23, 5),
+    ],
+)
+def test_pointsRounded(inValue, expected):
+    assert pointsRounded(inValue) == expected
 
 
 def ZZpointsRounded(inValue) -> int:
